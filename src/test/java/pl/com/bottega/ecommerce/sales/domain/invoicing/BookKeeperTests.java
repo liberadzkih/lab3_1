596a8dcb93cf4fd54c4503152bed292b6c6c4148
Barabasz.Rafal <215671@edu.p.lodz.pl>
2020-05-01 16:57:07 +0200

    Added 2 state tests of BookKeeper.

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTests.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTests.java
@@ -21,11 +21,13 @@ public class BookKeeperTests {
     BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
     InvoiceRequest invoiceRequest;
 
-    @Before public void initInvoiceRequest() {
+    @Before
+    public void initInvoiceRequest() {
         invoiceRequest = new InvoiceRequest(new ClientData(Id.generate(), "CBA"));
     }
 
-    @Test public void expectedInvoiceWithOneElement() {
+    @Test
+    public void expectedInvoiceWithOneElement() {
         Money money = new Money(BigDecimal.valueOf(111));
         Product product = new Product(Id.generate(), money, "Aspirin", ProductType.DRUG);
         ProductData productData = product.generateSnapshot();
@@ -39,7 +41,40 @@ public class BookKeeperTests {
         assertThat(1, equalTo(invoice.getItems().size()));
     }
 
-    @Test public void expectedTwoCallsOfCalculateTax() {
+    @Test
+    public void expectedInvoiceWithoutElements() {
+        TaxPolicy taxPolicyStub = Mockito.mock(TaxPolicy.class);
+        Mockito.when(taxPolicyStub.calculateTax(any(ProductType.class), any(Money.class)))
+               .thenReturn(new Tax(new Money(BigDecimal.valueOf(111)), "tax"));
+
+        Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicyStub);
+        assertThat(0, equalTo(invoice.getItems().size()));
+    }
+
+    @Test
+    public void expectedSpecificInvoiceCost() {
+        Money money1 = new Money(BigDecimal.valueOf(111));
+        Product product1 = new Product(Id.generate(), money1, "Aspirin", ProductType.DRUG);
+        ProductData productData1 = product1.generateSnapshot();
+
+        invoiceRequest.add(new RequestItem(productData1, 1, money1));
+
+        Money money2 = new Money(BigDecimal.valueOf(222));
+        Product product2 = new Product(Id.generate(), money2, "Banana", ProductType.FOOD);
+        ProductData productData2 = product2.generateSnapshot();
+
+        invoiceRequest.add(new RequestItem(productData2, 2, money2));
+
+        TaxPolicy taxPolicyStub = Mockito.mock(TaxPolicy.class);
+        Mockito.when(taxPolicyStub.calculateTax(any(ProductType.class), any(Money.class))).thenReturn(new Tax(money1, "Tax"));
+
+        Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicyStub);
+
+        assertThat(invoice.getNet(), equalTo(money1.add(money2)));
+    }
+
+    @Test
+    public void expectedTwoCallsOfCalculateTax() {
         Money money1 = new Money(BigDecimal.valueOf(111));
         Product product1 = new Product(Id.generate(), money1, "Aspirin", ProductType.DRUG);
         ProductData productData1 = product1.generateSnapshot();
