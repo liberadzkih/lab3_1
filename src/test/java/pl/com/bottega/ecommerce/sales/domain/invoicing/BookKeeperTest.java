88329c438526a8e3b46fb38541fa2986bbe1342a
Sebastian.Paweloszek <spaweloszek63@gmail.com>
2020-05-06 18:00:08 +0200

    test - should return invoice with one element

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,9 +1,12 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
@@ -19,8 +22,19 @@ public class BookKeeperTest {
     public void init() {
         bookKeeper = new BookKeeper(new InvoiceFactory());
         invoiceRequest = new InvoiceRequest(new ClientData(Id.generate(),"Imie Nazwisko"));
+        taxPolicy = Mockito.mock(TaxPolicy.class);
         Mockito.when(taxPolicy.calculateTax(any(ProductType.class),
                 any(Money.class))).thenReturn(new Tax(Money.ZERO, "tax"));
+    }
+
+    @Test
+    public void shouldReturnInvoiceWithOneElement() {
+        Product product = new Product(Id.generate(),Money.ZERO,"name",ProductType.STANDARD);
+        RequestItem requestItem = new RequestItem(product.generateSnapshot(),1, Money.ZERO);
+        invoiceRequest.add(requestItem);
+
+        Invoice invoice =bookKeeper.issuance(invoiceRequest, taxPolicy);
+        Assertions.assertEquals(1, invoice.getItems().size());
     }
 
 }
