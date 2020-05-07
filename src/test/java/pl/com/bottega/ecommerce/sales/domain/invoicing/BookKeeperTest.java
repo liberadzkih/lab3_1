7e8d88238825c22d19e6764c1eb961b874ea867b
Bilant.Andrzej <215679@edu.p.lodz.pl>
2020-05-07 13:35:12 +0200

    builder - Added Product and Request builders
    Signed-off-by: Bilant.Andrzej <215679@edu.p.lodz.pl>

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -3,10 +3,8 @@ package pl.com.bottega.ecommerce.sales.domain.invoicing;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
-import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
@@ -30,6 +28,7 @@ public class BookKeeperTest {
         bookKeeper = new BookKeeper(new InvoiceFactory());
         when(mockTaxPolicy.calculateTax(any(), any())).thenReturn(new Tax(new Money(BigDecimal.ONE), "Tax"));
     }
+
     //state tests
     @Test
     public void issuance_oneItem() {
@@ -37,13 +36,13 @@ public class BookKeeperTest {
         InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
 
         Money money = new Money(BigDecimal.ONE);
-        RequestItem requestItem = new RequestItem(new Product(Id.generate(), money, "Apple",
-                ProductType.FOOD).generateSnapshot(), 3, money);
+        RequestItem requestItem = new RequestItemBuilder().setProductData(new ProductBuilder().setAggregateId(Id.generate()).setPrice(money).setName("Apple").setProductType(ProductType.FOOD).createProduct().generateSnapshot()).setQuantity(3).setTotalCost(money).createRequestItem();
         invoiceRequest.add(requestItem);
 
         Invoice invoice = bookKeeper.issuance(invoiceRequest, mockTaxPolicy);
         assertEquals(1, invoice.getItems().size());
     }
+
     @Test
     public void issuance_zeroItems() {
         ClientData clientData = new ClientData(Id.generate(), "Client");
@@ -58,10 +57,12 @@ public class BookKeeperTest {
         ClientData clientData = new ClientData(Id.generate(), "Client");
         InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
 
-        for(int i=1;i<=5;i++) {
+        for (int i = 1; i <= 5; i++) {
             Money money = new Money(BigDecimal.ONE);
-            RequestItem requestItem = new RequestItem(new Product(Id.generate(), money, "Item"+i,
-                    ProductType.FOOD).generateSnapshot(), new Random().nextInt(15), money);
+            RequestItem requestItem = new RequestItemBuilder().setProductData(new ProductBuilder()
+                    .setAggregateId(Id.generate()).setPrice(money).setName("Item" + i).setProductType(ProductType.FOOD)
+                    .createProduct().generateSnapshot()).setQuantity(new Random().nextInt(15))
+                    .setTotalCost(money).createRequestItem();
             invoiceRequest.add(requestItem);
         }
         Invoice invoice = bookKeeper.issuance(invoiceRequest, mockTaxPolicy);
@@ -75,13 +76,15 @@ public class BookKeeperTest {
         InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
 
         Money money1 = new Money(BigDecimal.ONE);
-        RequestItem requestItem1 = new RequestItem(new Product(Id.generate(), money1, "Apple",
-                ProductType.FOOD).generateSnapshot(), 3, money1);
+        RequestItem requestItem1 = new RequestItemBuilder().setProductData(new ProductBuilder()
+                .setAggregateId(Id.generate()).setPrice(money1).setName("Apple").setProductType(ProductType.FOOD)
+                .createProduct().generateSnapshot()).setQuantity(3).setTotalCost(money1).createRequestItem();
         invoiceRequest.add(requestItem1);
 
         Money money2 = new Money(BigDecimal.TEN);
-        RequestItem requestItem2 = new RequestItem(new Product(Id.generate(), money2, "Apap",
-                ProductType.DRUG).generateSnapshot(), 2, money2);
+        RequestItem requestItem2 = new RequestItemBuilder().setProductData(new ProductBuilder()
+                .setAggregateId(Id.generate()).setPrice(money2).setName("Apap").setProductType(ProductType.DRUG)
+                .createProduct().generateSnapshot()).setQuantity(2).setTotalCost(money2).createRequestItem();
         invoiceRequest.add(requestItem2);
 
         bookKeeper.issuance(invoiceRequest, mockTaxPolicy);
@@ -100,14 +103,17 @@ public class BookKeeperTest {
         bookKeeper.issuance(invoiceRequest, mockTaxPolicy);
         verify(mockTaxPolicy, never()).calculateTax(any(ProductType.class), any(Money.class));
     }
+
     @Test
     public void issuance_moreThanThreeInvocations() {
         ClientData clientData = new ClientData(Id.generate(), "Client");
         InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
-        for(int i=1;i<=5;i++) {
+        for (int i = 1; i <= 5; i++) {
             Money money = new Money(BigDecimal.ONE);
-            RequestItem requestItem = new RequestItem(new Product(Id.generate(), money, "Item"+i,
-                    ProductType.FOOD).generateSnapshot(), new Random().nextInt(15), money);
+            RequestItem requestItem = new RequestItemBuilder().setProductData(new ProductBuilder()
+                    .setAggregateId(Id.generate()).setPrice(money).setName("Item" + i).setProductType(ProductType.FOOD)
+                    .createProduct().generateSnapshot()).setQuantity(new Random().nextInt(15))
+                    .setTotalCost(money).createRequestItem();
             invoiceRequest.add(requestItem);
         }
         bookKeeper.issuance(invoiceRequest, mockTaxPolicy);
