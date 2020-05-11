ed35c812e164e946253b0f677c06a5dcff52eedd
AdamWojtczak <adamwojtczak807@gmail.com>
2020-05-11 18:48:35 +0200

    Poprawka travis i gitignore
    Signed-off-by: AdamWojtczak <adamwojtczak807@gmail.com>

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -0,0 +1,12 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookKeeperTest {
+
+    @Test
+    public void issuance() {
+    }
+}
