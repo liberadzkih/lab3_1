e3a94671485d650b768c737cc70572640e872302
Tobiasz Skorża <209458@edu.p.lodz.pl>
2020-05-12 12:52:54 +0200

    Invoice get items should return proper information

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -9,6 +9,9 @@ import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.*;
@@ -82,12 +85,53 @@ class BookKeeperTest {
         clientData = new ClientData(Id.generate(),"client");
         invoiceRequest = new InvoiceRequest(clientData);
 
-        TaxPolicy taxPolicy = mock(TaxPolicy.class);
+        taxPolicy = mock(TaxPolicy.class);
         when(taxPolicy.calculateTax(ProductType.STANDARD, new Money(3))).thenReturn(new Tax(new Money(0.23),"23%"));
 
         Invoice invoice = bookKeeper.issuance(invoiceRequest,taxPolicy);
 
         Assertions.assertEquals(0,invoice.getItems().size());
+
+    }
+
+    @Test
+    public void invoiceGetItemsShouldReturnProperInformation(){
+
+        bookKeeper = new BookKeeper(new InvoiceFactory());
+        clientData = new ClientData(Id.generate(),"client");
+        invoiceRequest = new InvoiceRequest(clientData);
+
+        taxPolicy = mock(TaxPolicy.class);
+        when(taxPolicy.calculateTax(ProductType.STANDARD, new Money(3))).thenReturn(new Tax(new Money(0.23),"23%"));
+        when(taxPolicy.calculateTax(ProductType.FOOD, new Money(5))).thenReturn(new Tax(new Money(0.46),"46%"));
+
+        productData = mock(ProductData.class);
+        when(productData.getType()).thenReturn(ProductType.STANDARD);
+        when(productData.getName()).thenReturn("product1");
+
+        ProductData productData2 = mock(ProductData.class);
+        when(productData2.getType()).thenReturn(ProductType.FOOD);
+        when(productData2.getName()).thenReturn("product2");
+
+        RequestItem requestItem = new RequestItem(productData, 5, new Money(3));
+        RequestItem requestItem2 = new RequestItem(productData2,6, new Money(5));
+        invoiceRequest.add(requestItem);
+        invoiceRequest.add(requestItem2);
+
+        Invoice invoice = bookKeeper.issuance(invoiceRequest,taxPolicy);
+
+        List<String> productList = new ArrayList<>();
+        productList.add("product1");
+        productList.add("product2");
+
+        List<ProductType> productTypeList = new ArrayList<>();
+        productTypeList.add(ProductType.STANDARD);
+        productTypeList.add(ProductType.FOOD);
+
+        for(int i=0; i<productList.size(); i++){
+            Assertions.assertEquals(productList.get(i),invoice.getItems().get(i).getProduct().getName());
+            Assertions.assertEquals(productTypeList.get(i),invoice.getItems().get(i).getProduct().getType());
+        }
 
     }
 }
