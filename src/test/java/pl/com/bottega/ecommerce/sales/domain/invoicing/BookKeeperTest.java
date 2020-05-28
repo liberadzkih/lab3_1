6fa3c33604be517c7c4a0204ceb9e703ab54e4fb
Hubert Salamaga <215871@edu.p.lodz.pl>
2020-05-28 20:08:56 +0200

    Initialize BookKeeperTest class

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -0,0 +1,44 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BookKeeperTest {
+
+    @Mock
+    TaxPolicy taxPolicy;
+    ClientData clientData;
+    InvoiceRequest invoiceRequest;
+    BookKeeper bookKeeper;
+    Product product;
+    ProductData productData;
+    Invoice invoice;
+    InvoiceFactory invoiceFactorySpy;
+
+    @Before
+    public void init() {
+        clientData = new ClientData(Id.generate(), "Bob");
+        invoiceRequest = new InvoiceRequest(clientData);
+        bookKeeper = new BookKeeper(new InvoiceFactory());
+        product = new Product(Id.generate(), new Money(13.37), "Peanut butter", ProductType.FOOD);
+        productData = product.generateSnapshot();
+        invoiceFactorySpy = Mockito.spy(new InvoiceFactory());
+        when(taxPolicy.calculateTax(any(ProductType.class), any(Money.class))).thenReturn(new Tax(new Money(21.37), "Tax Description"));
+    }
+}
