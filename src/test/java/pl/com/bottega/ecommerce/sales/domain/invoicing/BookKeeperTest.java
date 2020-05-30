3251a1acf8242fea8765c318e15740b6f7f9b728
DGromek <dawidgromek.1997@gmail.com>
2020-05-30 15:01:10 +0200

    Added 2 new Behaviour tests

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
@@ -14,8 +15,7 @@ import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -30,17 +30,19 @@ public class BookKeeperTest {
     @Mock
     private TaxPolicy taxPolicy;
     private ProductData productData;
+    @Spy
+    private InvoiceFactory invoiceFactory;
 
     @BeforeEach
     public void setUp() {
-        bookKeeper = new BookKeeper(new InvoiceFactory());
+        bookKeeper = new BookKeeper(invoiceFactory);
         invoiceRequest = new InvoiceRequest(clientData);
 
         productData = new ProductBuilder()
                 .build()
                 .generateSnapshot();
 
-        when(taxPolicy.calculateTax(any(ProductType.class), any(Money.class))).thenReturn(new Tax(UNRELEVANT_MONEY, "unrelevant"));
+        lenient().when(taxPolicy.calculateTax(any(ProductType.class), any(Money.class))).thenReturn(new Tax(UNRELEVANT_MONEY, "unrelevant"));
     }
 
     //State tests
@@ -82,5 +84,21 @@ public class BookKeeperTest {
 
         verify(taxPolicy).calculateTax(requestItem1.getProductData().getType(), requestItem1.getTotalCost());
         verify(taxPolicy).calculateTax(requestItem2.getProductData().getType(), requestItem2.getTotalCost());
+    }
+
+    @Test
+    void invoiceRequestWith0PositionsShouldCalculateTax0Times() {
+        bookKeeper.issuance(invoiceRequest, taxPolicy);
+
+        verify(taxPolicy, never()).calculateTax(any(ProductType.class), any(Money.class));
+    }
+
+    @Test
+    void invoiceRequestShouldCreateOneInvoice() {
+        invoiceRequest.add(new RequestItem(productData, UNRELEVANT_QUANTITY, UNRELEVANT_MONEY));
+        invoiceRequest.add(new RequestItem(productData, UNRELEVANT_QUANTITY, UNRELEVANT_MONEY));
+
+        bookKeeper.issuance(invoiceRequest, taxPolicy);
+        verify(invoiceFactory).create(invoiceRequest.getClient());
     }
 }
