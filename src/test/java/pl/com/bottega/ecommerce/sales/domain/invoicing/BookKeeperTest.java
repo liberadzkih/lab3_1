6ea6d79f75559643ee4ab281136bac868f3367d8
liberadzkih <liberadzkih@gmail.com>
2020-09-13 22:59:15 +0200

    Added test case 2

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
@@ -36,10 +37,27 @@ public class BookKeeperTest {
     @Test
     public void invoiceIssuanceRequest_oneItem() {
         InvoiceRequest invoiceRequest = new InvoiceRequest(clientDataMock);
-        RequestItem requestItem = new RequestItem(new Product(Id.generate(), amount, "cigarettes", ProductType.DRUG).generateSnapshot(), 1, amount);
+        ProductData productData = new Product(Id.generate(), amount, "cigarettes", ProductType.DRUG).generateSnapshot();
+        RequestItem requestItem = new RequestItem(productData, 1, amount);
         invoiceRequest.add(requestItem);
         Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicyMock);
         assertEquals(1, invoice.getItems().size());
+    }
+
+    //Test case 2 żądanie wydania faktury z dwiema pozycjami powinno wywołać metodę calculateTax dwa razy
+    @Test
+    public void invoiceIssuanceRequest_twoItems() {
+        InvoiceRequest invoiceRequest = new InvoiceRequest(clientDataMock);
+        ProductData productData = new Product(Id.generate(), amount, "cigarettes", ProductType.DRUG).generateSnapshot();
+        RequestItem requestItem = new RequestItem(productData, 1, amount);
+        invoiceRequest.add(requestItem);
+
+        productData = new Product(Id.generate(), amount, "banana", ProductType.FOOD).generateSnapshot();
+        requestItem = new RequestItem(productData, 1, amount);
+        invoiceRequest.add(requestItem);
+
+        Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicyMock);
+        assertEquals(2, invoice.getItems().size());
     }
 
 }
